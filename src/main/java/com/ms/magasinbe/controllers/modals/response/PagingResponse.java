c7ff0d258c76394cf23eb50d2a1fd405cb746b18
duyduc-dev <dangduyducdangduyduc@gmail.com>
2023-09-02 22:42:05 +0700

    chore

---
+++ src/main/java/com/ms/magasinbe/controllers/modals/response/PagingResponse.java
@@ -0,0 +1,43 @@
+package com.ms.magasinbe.controllers.modals.response;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+/**
+ * @Author: Dang Duy Duc - Magasin
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PagingResponse {
+  private List<?> content;
+  private long totalElements;
+  private int numberOfElements;
+  private int pageSize;
+  private int pageNumber;
+  private long totalPages;
+
+  public PagingResponse(Page<?> page) {
+    this.content = page.getContent();
+    this.totalElements = page.getTotalElements();
+    this.numberOfElements = page.getNumberOfElements();
+    this.pageSize = page.getSize();
+    this.pageNumber = page.getNumber() + 1;
+    this.totalPages = page.getTotalPages();
+  }
+
+  public PagingResponse(List<?> content, Page<?> page) {
+    this.content = content;
+    this.totalElements = page.getTotalElements();
+    this.numberOfElements = page.getNumberOfElements();
+    this.pageSize = page.getSize();
+    this.pageNumber = page.getNumber() + 1;
+    this.totalPages = page.getTotalPages();
+  }
+
+}
